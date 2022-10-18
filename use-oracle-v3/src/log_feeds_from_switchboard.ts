d8f6e75e66436bab64923b9e96d1b14a887bf400
linnefromice <linnefromice@gmail.com>
2022-10-18 23:22:55 +0900

    update

--- use-oracle-v3/src/log_feeds_from_switchboard.ts
+++ use-oracle-v3/src/log_feeds_from_switchboard.ts
@@ -1,11 +1,44 @@
+import fs from "fs";
 import { AptosClient } from "aptos";
-import { NODE_URL, } from "./utils/env";
+import { NODE_URL } from "./utils/env";
 import { aggregators } from "./utils/env_switchboard";
 import { currentRound } from "./utils/log_resource";
 
+const logCurrentRound = (
+  name: string,
+  datetime: string,
+  data: Awaited<ReturnType<AptosClient["getAccountResource"]>>
+) => {
+  fs.writeFileSync(
+    `temp/currentRound-${name}-${datetime}.log`,
+    JSON.stringify(data, null, 2),
+    { flag: "a" }
+  );
+}
+const logLatestConfirmedRound = (
+  name: string,
+  datetime: string,
+  data: Awaited<ReturnType<AptosClient["getAccountResource"]>>
+) => {
+  fs.writeFileSync(
+    `temp/latestConfirmedRound-${name}-${datetime}.log`,
+    JSON.stringify(data, null, 2),
+    { flag: "a" }
+  );
+}
+
 (async () => {
   const client = new AptosClient(NODE_URL)
-  const resource = await currentRound(client, aggregators.btcusd)
-  // console.log(JSON.stringify(resource, null, 2))
-  console.log(JSON.stringify(resource["data"], null, 2))
+  
+  const currentDatetime = new Date().toISOString().replace(/(-|T|:)/g, "").substring(0, 14)
+  logCurrentRound(
+    "usdcusd",
+    currentDatetime,
+    await currentRound(client, aggregators.usdcusd)
+  )
+  logCurrentRound(
+    "btcusd",
+    currentDatetime,
+    await currentRound(client, aggregators.btcusd)
+  )
 })();
